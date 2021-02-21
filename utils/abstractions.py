c8049583bde09cf2594d872b87e4d713a2df9a37
latticetower <merlettaia@gmail.com>
2021-02-21 23:14:28 +0300

    update methods to extract stations and orders

--- utils/abstractions.py
+++ utils/abstractions.py
@@ -53,32 +53,40 @@ def get_roads_graph(metrics, debug=True):
     return graph, name2vertex
 
 
-def init_stations(sources, name2vertex=dict()):
-    """name2vertex gets modified"""
-    columns = ["Date", "Units", "Stand", "WagonModel"]
-    stations = dict() # station by name
-    max_date = sources.Date.max()
-    new_vertices = 0
-    station_days = dict()
-    for s, data in sources.groupby("Station"):
-        s = str(s)
-        if s not in name2vertex:
-            i = len(name2vertex)
-            name2vertex[s] = i
-            new_vertices += 1
-        else:
-            i = name2vertex[s]
-        if i not in station_days:
-            station_days[i] = []
-        for d, u, c, wm in data[columns].values:
-            station_days[i].append((d, u, c, wm))
-    print(new_vertices)
+def create_station_graph(sources, name2vertex):
+    #print(sources)
+    columns = ["Units", "Stand", "WagonModel"]
     graph = Graph()
     graph.add_vertices(len(name2vertex))
-    for i, j in name2vertex.items():
-        graph.vs[j]['name'] = i
-        graph.vs[j]['days'] = station_days.get(j, [])
+    graph.vs['info'] = [dict()]*len(name2vertex)
+    for station, row in sources.groupby("Station"):
+        # print(station)
+        station = str(station)
+        i = name2vertex[station]
+        wmodels = dict()
+        for units, stand, wm in row[columns].values:
+            (wm, units, stand)
+            if wm not in wmodels:
+                wmodels[wm] = []
+            wmodels[wm].append((units, stand))
+        graph.vs[i]['info'] = wmodels
     return graph
+
+
+def init_stations(sources, name2vertex):
+    """name2vertex gets modified"""
+    columns = ["Station", "Date", "Units", "Stand", "WagonModel"]
+    for station in np.unique(sources['Station'].values):
+        station = str(station)
+        if station not in name2vertex:
+            index = len(name2vertex)
+            name2vertex[station] = index
+    graphs = dict()
+    for d, arrivals in sources[columns].groupby("Date"):
+        graph = create_station_graph(arrivals, name2vertex)
+        # print(graph)
+        graphs[d] = graph
+    return graphs
 
 
 def get_wagon_graph(wagon_mode_compat):    
@@ -99,6 +107,70 @@ def get_wagon_graph(wagon_mode_compat):
     edges = sorted(edges)
     graph.add_edges(edges)
     return graph, wagon2vertex
+
+
+def create_order_graph(orders, name2vertex):
+    columns = [
+        "OrderNum",
+        "Start",
+        "Finish",
+        #"StartDate",
+        "Dur",
+        "MinUnit",
+        "MaxUnit",
+        "Tariff",
+        "NeedWagonModel",
+        "ShortagePenalty"
+    ]
+    vertices = np.unique(np.concatenate([
+        orders.Start.values,
+        orders.Finish.values
+    ]))
+    for v in vertices:
+        v = str(v)
+        if v not in name2vertex:
+            index = len(name2vertex)
+            name2vertex[v] = index
+    graph = Graph()
+    graph.add_vertices(len(name2vertex))
+    edges = []
+    for start, finish in orders[["Start", "Finish"]].values:
+        start = str(start)
+        finish = str(finish)
+        u = name2vertex[start]
+        v = name2vertex[finish]
+        edges.append((u, v))
+    # print(edges)
+    graph.add_edges(edges)
+    graph['order_num'] = orders["OrderNum"].values
+    graph['dur'] = orders["Dur"].values
+    graph['min_unit'] = orders["MinUnit"].values
+    graph['max_unit'] = orders["MaxUnit"].values
+    graph['tariff'] = orders["Tariff"].apply(lambda xs: [int(x) for x in xs.split(":")]).values
+    graph["wagon_model"] = orders["NeedWagonModel"].values
+    graph["penalty"] = orders["ShortagePenalty"].values
+    return graph
+
+
+def build_orders(orders, name2vertex):
+    columns = [
+        "OrderNum",
+        "Start",
+        "Finish",
+        "StartDate",
+        "Dur",
+        "MinUnit",
+        "MaxUnit",
+        "Tariff",
+        "NeedWagonModel",
+        "ShortagePenalty"
+    ]
+    graphs = dict()
+    for start_date, rows in orders7[columns].groupby("StartDate"):
+        graph = create_order_graph(rows, name2vertex)
+        graphs[start_date] = graph
+    # return start_date, rows
+    return graphs
 
 
 @dataclass
