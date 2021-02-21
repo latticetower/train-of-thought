c358825ef000eca44bbed664b55b19887410b68e
lacemaker <merlettaia@gmail.com>
2021-02-21 20:18:59 +0000

    add Dima's recent changes to code

--- utils/common.py
+++ utils/common.py
@@ -42,25 +42,47 @@ class WagonManager(object):
     def __init__(self):
         self.wagons_by_id = {}
         self.wagons_by_loc = {}
+        self.wagons_by_loc_rev = {}
+
 
     def add_wagon(self, wagon):
         self.wagons_by_id[wagon.index] = wagon
-        self.wagons_by_loc[wagon.current_station] = wagon.index 
+        if wagon.current_station not in self.wagons_by_loc:
+            self.wagons_by_loc[wagon.current_station] = set()     
+        self.wagons_by_loc[wagon.current_station].add(wagon.index) 
+        self.wagons_by_loc_rev[wagon.index] = wagon.current_station
     
     def wagon_number(self):
         return len(self.wagons_by_id)
 
 
 class Strategy(object):
-    def __init__(self, order_manager, wagon_manager, graph, compability_model, empty_costs):
+    def __init__(self, order_manager, wagon_manager, graph, compability_model):
         self.order_manager = order_manager
         self.wagon_manager = wagon_manager
         self.graph = graph
         self.compability_model = compability_model
-        self.empty_costs = empty_costs
         self.total_cost = 0
-
+        self.current_order = 0
     
     def are_compatible(self, type1, type2):
         return (str(type1) + "_" + str(type2)) in self.compability_model
+
+    def start_order(self, order):
+        wagons_to_send = []
+        for wagon in self.wagon_manager.wagons_by_loc[order.start_point]:
+           if not wagon.occupied and self.are_compatible(wagon.type_of_wagon, order.wagon_type):
+               wagons_to_send.append(wagon)
+               if len(wagons_to_send) == order.wagons:
+                   break
+        if  len(wagons_to_send) < order.wagons:
+            print("Problem")
+            return
+        
+        for wagon in wagons_to_send:
+
+    def run_day(self, day_num):
+        while self.current_order < len(self.order_manager.orders) and self.order_manager.orders[self.current_order] == day_num:
+            self.start_order(self.order_manager.orders[self.current_order])
+            self.current_order += 1
 
